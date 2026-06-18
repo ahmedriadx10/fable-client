cb4dd9d24603477f4975e1ac82ba1ad93103b7e3
ahmedriadx10 <infinitecoderx02@gmail.com>
2026-06-18 11:47:43 +0600

    fable client important dependencies installed

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,10 @@
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <h2>Register page</h2>
+    </div>
+  );
+};
+
+export default RegisterPage;
---
+++ src/app/(public)/page.jsx
@@ -0,0 +1,10 @@
+
+const HomePage = () => {
+  return (
+    <div>
+      <h2>Homepage here</h2>
+    </div>
+  );
+};
+
+export default HomePage;
